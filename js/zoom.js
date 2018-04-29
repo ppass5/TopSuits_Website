7c9cb4815e67812167517db313a4ebf7de7b030e
ppass5 <lena.m.gonzalez@gmail.com>
2018-04-29 15:34:45 -0400

    changed some of the sections

--- js/zoom.js
+++ js/zoom.js
@@ -5,8 +5,12 @@ $(window).scroll(function(){
     var $maxScroll=600;
     var $maxScale=1.3;
     var $x=$(window).scrollTop()/100+1;
-    if($(window).scrollTop()>$maxScroll) $x=$maxScale;
-//    $('#first .background').css({transform: 'scale('+$x+','+$x+')'});
-        $('#first .astro').css({transform: 'scale('+$x+','+$x+')'});
+    // var $x=$('html, body').animate({scrollTop: $('#welcome').offset().top -100 }, 'slow');
+
+    /*this statment scrolls out back in again when max is reached*/
+   if($(window).scrollTop()>$maxScroll) $x=$maxScale;
+   /*this statment scrolls out when max is reached*/
+
+    $('#first .astro').css({transform: 'scale('+$x+','+$x+')'});
 
 });
