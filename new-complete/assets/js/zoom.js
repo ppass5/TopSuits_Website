a923162a50f62546917f0d61a7ef47e83c13e7b1
ppass5 <lena.m.gonzalez@gmail.com>
2018-05-02 11:34:37 -0400

    added-profile/zoom js files

---
+++ new-complete/assets/js/zoom.js
@@ -0,0 +1,16 @@
+$('#first').css({background:'none'});
+
+/*Scrolling*/
+$(window).scroll(function(){
+    var $maxScroll=600;
+    var $maxScale=1.3;
+  var $x=$(window).scrollTop()/100+1;
+   // var $x=$('html, body').animate({scrollTop: $('#welcome','#teams').offset().top -100 }, 'slow'); /*possible scroll to welcome id*/
+
+    /*this statment scrolls out back in again when max is reached*/
+   // if($(window).scrollTop()>$maxScroll) $x=$maxScale;
+   /*this statment scrolls out when max is reached*/
+
+    $('#first .astro').css({transform: 'scale('+$x+','+$x+')'});
+
+});
