cf4a6b6637d2c1a5832552b6cdfd775e66223ba1
ppass5 <lena.m.gonzalez@gmail.com>
2018-05-05 11:14:42 -0400

    deleted/changed items

--- js/profile.js
+++
@@ -1,6 +0,0 @@
-$('.flip-container').mouseenter(function(){
-  $('body').addClass('body_m', 1000);
-});
-$('.flip-container').mouseleave(function(){
-  $('body').stop().removeClass('body_m', 900);
-});
